aba19105cf4b09ad9c3cff98c7b40ebcddefa153
DucVu1 <ducvmakb@gmail.com>
2024-04-30 18:28:34 +0700

    small update

--- src/Immediate_generator.sv
+++ src/Immediate_generator.sv
@@ -3,12 +3,12 @@ module Immediate_generator #(parameter Width = 32)(
 	output logic [Width-1:0] Out);
 always_comb begin
 	case(In[6:0])
-		7'b0000011 : Out = {{(32-12){In[31]}},In[31:20]};
-		7'b0100011 : Out = {{(32-12){In[31]}},In[31:25],In[11:7]};
-		7'b1100011 : Out = {{(32-12){In[31]}},In[31],In[7],In[30:25],In[11:8]};
-		7'b0010011 : Out = {{(32-12){In[31]}},In[31:20]};
-		7'b1100111 : Out = {{(32-12){In[31]}},In[31:20]};
-		7'b1101111 : Out = {{(32-20){In[31]}},In[31],In[30:21],In[20],In[19:12]};
+		7'b0000011 : Out = {{(32-12){In[31]}},In[31:20]}; //I-type for load Instruction 
+		7'b0100011 : Out = {{(32-12){In[31]}},In[31:25],In[11:7]}; //S-type Instruction
+		7'b1100011 : Out = {{(32-12){In[31]}},In[31],In[7],In[30:25],In[11:8]}; //B-type Instruction 
+		7'b0010011 : Out = {{(32-12){In[31]}},In[31:20]}; //I-type for arithmetic Instruction
+		7'b1100111 : Out = {{(32-12){In[31]}},In[31:20]}; // Jump 
+		7'b1101111 : Out = {{(32-20){In[31]}},In[31],In[19:12],In[20],In[30:21]};//Jump and Link
 	default : Out = 32'd0;
 endcase
 end
--- src/register_file.sv
+++ src/register_file.sv
@@ -1,23 +1,34 @@
- module register_file#(parameter DATA_N = 32,SIZE = 32)
- (
-	 input logic clk, wr_en,
-	 input logic [4:0] w_addr, r0_addr, r1_addr,
-	 input logic [DATA_N-1:0] w_data,
-	 output logic [DATA_N-1:0] r8,r11,
-	 output logic [DATA_N-1:0] r0_data, r1_data
- );
- 
+module register_file #(parameter DATA_N = 32, SIZE = 32)
+(
+    input logic clk, wr_en,
+    input logic [4:0] w_addr, r0_addr, r1_addr,
+    input logic [DATA_N-1:0] w_data,
+    output logic [DATA_N-1:0] r8, r11,
+    output logic [DATA_N-1:0] r0_data, r1_data
+);
+logic [4:0] ignor_regs = 5'd0;
 logic [DATA_N-1:0] regs [0:SIZE-1];
 
-assign r8 = regs[8];
-assign r11 = regs[11];
+ // Initial block to assign initial value to regs[11]
+initial begin
+	for(int i = 0; i<32; i++) begin 
+		if(i == 11) 
+			regs[i] = 32'd5;
+		else
+			regs[i] = 32'd0;
+			end
+end
 
-always_ff @(posedge clk) begin 
-	if (wr_en) begin
-		regs[w_addr] <= w_data; end
-		end
-		
+always_ff @(posedge clk) begin
+  if (wr_en) begin
+		regs[w_addr] <= (w_addr == ignor_regs) ? 32'd0 : w_data;
+  end
+end
+
+assign r8 = regs[5'd8];
+assign r11 = regs[5'd11];
+
 assign r0_data = regs[r0_addr];
 assign r1_data = regs[r1_addr];
- 
-endmodule 
+
+endmodule
